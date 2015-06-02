d04f9cb457ebe8dcf2cbc299208e9b47c6d19572
RobPavl <robert.pavlovsky@gmail.com>
2015-06-02 15:25:03 +0300

    test "invalid without a title" green

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'invalid without a title' do
+  	question = Question.new(title: nil)
+  	question.valid?
+  	expect(question.errors[:title]).to include("can't be blank")
+  end
+
 end
