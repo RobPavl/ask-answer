4fb40d38c7eac758507a5783fd02506a1591578a
RobPavl <robert.pavlovsky@gmail.com>
2015-06-04 14:43:11 +0300

    added separation between conversations and validation in models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
+  
   validates :title, :body, presence: true
   validates :title, length: 5..140
   validates :body, length: 15..1050
