57e94f4a7a8ed6db30c19e0736da3d698b6852dd
RobPavl <robert.pavlovsky@gmail.com>
2015-06-04 15:07:22 +0300

    fixes in spec/models

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,18 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it {should validate_presence_of(:title)}
-  it {should validate_presence_of(:body)}
-  it {should have_many :answers}
-  it {should have_many(:answers).dependent(:destroy)}
-  it do
-    should validate_length_of(:title)
-      .is_at_least(5)
-      .is_at_most(140)
-  end
-  it do
-    should validate_length_of(:body)
-      .is_at_least(15)
-      .is_at_most(1050)
-  end
+  it { should have_many(:answers).dependent(:destroy) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_length_of(:title).is_at_least(5).is_at_most(140) }
+  it { should validate_length_of(:body).is_at_least(15).is_at_most(1050) }
 end
