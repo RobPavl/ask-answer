6d3b432025812c4163605f0d387940ce1c64b0cd
RobPavl <robert.pavlovsky@gmail.com>
2015-06-11 13:51:55 +0300

    created action get#edit for questions and tested them

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,4 +11,8 @@ class QuestionsController < ApplicationController
 	def new
 		@question = Question.new
 	end
+
+	def edit
+	 	@question = Question.find(params[:id])
+	end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -16,7 +16,7 @@ require 'rails_helper'
 	    end
 	  end
 
-	describe 'GET #show' do
+	  describe 'GET #show' do
 	    let(:question) { create(:question) }
 	    
 	    before { get :show, id: question }
@@ -27,9 +27,9 @@ require 'rails_helper'
 	    it 'renders show view' do
 	      expect(response).to render_template :show
 	    end
-	end
+	  end
   
-	describe 'GET #new' do
+	  describe 'GET #new' do
 		before { get :new }
 
 		it 'get a new question object' do
@@ -37,7 +37,20 @@ require 'rails_helper'
 		end
 
 		it { should render_template :new }
-	end
+	  end
+
+	  describe 'GET #edit' do
+	  	let(:question) { create(:question) }
+	  	
+	  	before { get :edit, id: question }
+
+	  	it 'assings the requested question to @question'do
+	  	  expect(assigns(:question)).to eq question
+	    end
+	    it 'renders edit view' do
+	      expect(response).to render_template :edit
+	    end
+	  end
 
 	end
   
